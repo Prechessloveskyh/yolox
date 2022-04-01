2504b7096a6cf5f5a14fa9a1c14b52efad8bf47c
vladyslav.marchenko <vladyslav.marchenko@chisw.com>
2022-04-01 19:07:10 +0300

    focal loss has been added

--- yolox/models/yolo_head.py
+++ yolox/models/yolo_head.py
@@ -148,9 +148,9 @@ class YOLOXHead(nn.Module):
             )
 
         self.use_l1 = False
-        self.l1_loss = nn.L1Loss(reduction="mean")
-        self.bcewithlog_loss = FocalLoss(nn.BCEWithLogitsLoss(reduction="mean"))
-        self.iou_loss = IOUloss(reduction="mean")
+        self.l1_loss = nn.L1Loss(reduction="none")
+        self.bcewithlog_loss = FocalLoss(nn.BCEWithLogitsLoss(reduction="none"))
+        self.iou_loss = IOUloss(reduction="none")
         self.strides = strides
         self.grids = [torch.zeros(1)] * len(in_channels)
         self.expanded_strides = [None] * len(in_channels)
